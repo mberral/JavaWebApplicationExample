f2f298643167133496775dedf89ee273f9856372
Miguel Berral <miguel.13bd@hotmail.com>
2024-10-13 20:27:39 +0200

    Se ha añadido un formulario en el servlet principal que recoge el nombre y los apellidos para enviarlos al segundo servlet y mostrarlo por pantalla.

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -19,12 +19,42 @@ public class MyServlet extends HttpServlet {
 
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head>");
+		out.println("<title>Libro de visitas</title>");
+		out.println("<style>");
+		out.println("html { background: rgb(2,0,36); background: linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%); }");
+		out.println("body { font-family: Arial; width: 600px; margin: 80px auto; }");
+		out.println("header { text-align: center; color: white; }");
+		out.println("div { border-radius: 15px; background-color: silver; padding: 20px; }");
+		out.println("input { width: 100%; padding: 12px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box; }");
+		out.println("input[type=submit] { background-color: #00CC00; width: 100px; color: #fff; }");
+		out.println("input[type=submit]:hover { background-color: #33CC00; border-color: #33CC33; color: #f4f6f7; }");
+		out.println("input[type=reset] { background-color: #fff; border-color: #dadce0; color: #1a73e8; width: 100px; }");
+		out.println("input[type=reset]:hover { background-color: #f6f9fe; border-color: #1a73e8; color: #174ea6; }");
+		out.println("</style>");
+		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>Ejemplo Servlet</h1>");
-		out.println("<p>Este es un ejemplo en el curso de Java para generar HTML desde un Servlet.</p>");
-		out.println("<p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
-		out.println("</body></html>");
+		out.println("<header>");
+		out.println("<h1>¡Bienvenido/a al libro de visitas!</h1>");
+		out.println("<p>Deja tu visita registrada</p>");
+		out.println("</header>");
+		out.println("<br>");
+		out.println("<br>");
+		out.println("<div>");
+		out.println("<form action=\"/myServlet2\">");
+		out.println("<label for=\"fNombre\">Nombre:</label> <br>");
+		out.println("<input type=\"text\" id=\"fNombre\" name=\"fNombre\">");
+		out.println("<br>");
+		out.println("<br>");
+		out.println("<label for=\"fApellidos\">Apellidos:</label><br>");
+		out.println("<input type=\"text\" id=\"fApellidos\" name=\"fApellidos\">");
+		out.println("<br>");
+		out.println("<br>");
+		out.println("<input type=\"submit\" value=\"FIRMAR\"> <input type=\"reset\" value=\"BORRAR\">");
+		out.println("</form>");
+		out.println("</div>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
--- src/main/java/ejemplos/servlet/curso/MyServlet2.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -17,11 +17,22 @@ public class MyServlet2 extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
+		String fNombre = request.getParameter("fNombre");
+		String fApellidos = request.getParameter("fApellidos");
+
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head>");
+		out.println("<title>¡Gracias por tu visita!</title>");
+		out.println("<style>");
+		out.println("html { background: rgb(2,0,36); background: linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%); }");
+		out.println("body { font-family: Arial; width: 600px; margin: 80px auto; color: #fff }");
+		out.println("</style>");
+		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>Bienvenido!!</h1>");
+		out.println("<h1>Tu visita se ha registrado " + fNombre + " " + fApellidos + "</h1>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
